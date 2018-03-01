d7fbe7c144e55ef856ac8957d4bd04549798ba37
Marcia Cavalcante <marciacavalcante@MacBook-Pro-de-Marcia.local>
2018-03-01 14:31:28 -0300

    Asterisk Generator Class and Test

---
+++ Introduction/src/asterisk/AsteriskGenerator.java
@@ -0,0 +1,5 @@
+package asterisk;
+
+public class AsteriskGenerator {
+
+}
---
+++ Introduction/test/asterisk/AsteriskGeneratorTest.java
@@ -0,0 +1,15 @@
+package asterisk;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AsteriskGeneratorTest {
+
+    @Test
+    public void shouldPrintOneAsterisk(){
+
+
+    }
+
+}
