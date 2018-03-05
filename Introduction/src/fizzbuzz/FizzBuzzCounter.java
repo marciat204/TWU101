2fba75fc2cde4bb3c97ddbe2c5036d790a82c219
Marcia Cavalcante <marcia.cavalcante7@gmail.com>
2018-03-05 11:25:31 -0300

    FizzBuzz Counter

--- Introduction/src/fizzbuzz/FizzBuzzCounter.java
+++ Introduction/src/fizzbuzz/FizzBuzzCounter.java
@@ -1,7 +1,28 @@
 package fizzbuzz;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class FizzBuzzCounter {
     public String print(int numbers) {
-        return null;
+        List<String> printedNumbersBuffer = new LinkedList<>();
+
+        for (int i = 1; i <= numbers; i++) {
+            String currentNumber = "";
+            if (i % 3 == 0 || i % 5 == 0){
+                if (i % 3 == 0){
+                    currentNumber = currentNumber.concat("Fizz");
+                }
+                if (i % 5 == 0) {
+                    currentNumber = currentNumber.concat("Buzz");
+                }
+            }
+            else {
+                currentNumber = "" + i;
+            }
+            printedNumbersBuffer.add(currentNumber);
+        }
+        String printedNumbers = String.join("\n", printedNumbersBuffer);
+        return printedNumbers;
     }
 }
